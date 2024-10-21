9c8ab2416151168f10ce29bb03f8aafe4cf2a84e
LeventeVeresBocskay <vblevi1@gmail.com>
2024-10-21 21:28:50 +0200

    Finished login page and php

--- root/pages/login.php
+++ root/pages/login.php
@@ -3,14 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-    <link rel="stylesheet" href="../css/login.css">
+    <title>LoginPHP</title>
 </head>
 <body>
-    <div class="Container">
-        <div class="Border">
-            <img src="../images/intranet.jpg" alt="Intranet" class="Image">
-        </div>
-    </div>
+<?php
+
+$valid_email = "user@user.com";
+$valid_password = "password";
+
+
+$email = $_POST['emailaddress'];
+$password = $_POST['password'];
+
+
+if ($email === $valid_email && $password === $valid_password) {
+    echo "<h2>Login successful! Welcome, $email.</h2>";
+    echo "<a href='index.php'>Go back to home page</a>"; 
+} else {
+    echo "<h2>Invalid username or password.</h2>";
+    echo "<a href='index.php'>Try again</a>";
+}
+?>
+<!-- Above add go back to home page hyperlink -->
 </body>
 </html>
