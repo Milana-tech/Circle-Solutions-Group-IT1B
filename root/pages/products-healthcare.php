f19eb42a5a0eb01036deac1ced251acdc6f636a1
RodrigoCardoso-10 <rodricardoso7@outlook.com>
2024-10-22 05:20:54 +0200

    worked on the sign up page and tried to fix the width of the healthcare and education products pages unsuccessfully

---
+++ root/pages/products-healthcare.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="../css/products-healthcare.css" type="text/css"/>
+  <link rel="stylesheet" href="../css/global.css" type="text/css">
+  <title>Healthcare | Circle Solutions</title>
+</head>
+
+<?php
+include "/app/public/products_rodrigo/Milana-tech-Circle-Solutions-Group-IT1B/root/sections/header.html";
+?>
+
+<body>
+  <div class="container">
+    <h1>Healthcare</h1>
+
+    <!-- Comms Section -->
+    <div class="comms-section">
+      <div class="comms-image">
+        <img src="../images/healthcare-comms.png" alt="comms image" />
+      </div>
+      <div class="comms-text">
+        <p>
+        <h2><i>Simplify comms work</i></h2>
+        </p>
+        <p>
+          Easily plan and distribute you communications using a
+          shared editorial calendar, collaborative workflow and
+          single-click multichannel distribution.
+        </p>
+      </div>
+    </div>
+
+    <!-- Efficiency Section -->
+    <div class="efficiency-section">
+      <div class="efficiency-text">
+        <p>
+        <h2><i>Increase employee efficiency</i></h2>
+        </p>
+        <p>
+          Make life easier for on-the-move employees with centralized content,
+          real-time updates on operational disruptions, and front door access to systems and services.
+        </p>
+      </div>
+      <div class="efficiency-image">
+        <img src="../images/efficiency.png" alt="efficiency image" />
+      </div>
+    </div>
+
+    <!-- boxes section -->
+    <section class="parent">
+
+      <!-- gold -->
+      <section class="child-left">
+        <br>
+        <center>
+          <h4>Gold Plan - 180.00€ p/month</h4>
+        </center> <br>
+
+        <ul>
+          <li><i>Single-click multichannel distribution</i></li>
+          <li><i>Access to system and services</i></li>
+          <li><i>Collaborative workflow</i></li>
+          <li><i>Limited users (up to 50)</i></li>
+        </ul>
+        <div class="section-buttons">
+          <button class="cart-btn">Add to cart</button>
+          <button class="buy-btn">Buy now</button>
+        </div>
+        </p>
+      </section>
+
+      <!-- platinum -->
+      <section class="child-right">
+        <p><br>
+          <center>
+            <h4>Platinum plan - 760.00€ p/month</h4>
+          </center><br>
+        <ul>
+          <li><i>Single-click multichannel distribution</i></li>
+          <li><i>Access to system and services</i></li>
+          <li><i>Collaborative workflow</i></li>
+          <li><i>Limited users (up to 500)</i></li>
+        </ul>
+        <div class="section-buttons">
+          <button class="cart-btn">Add to cart</button>
+          <button class="buy-btn">Buy now</button>
+        </div>
+        </p>
+      </section>
+    </section>
+  </div>
+</body>
+
+<?php
+include "/app/public/products_rodrigo/Milana-tech-Circle-Solutions-Group-IT1B/root/sections/footer.html";
+?>
+
+</html>
