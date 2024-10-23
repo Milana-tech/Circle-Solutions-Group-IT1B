ba3a3e43ea0d588571c8cd7af4a93454cf94ea41
Pharrellstu <pharrell.buckman@student.nhlstenden.com>
2024-10-23 16:18:47 +0200

    Added header and footer and gave unique classes

---
+++ root/pages/about-us.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/global.css">
+</head>
+<body class="aboutbody">
+    <div class="main">
+        <section class="navbar">
+            <?php
+            include "../sections/header.html";
+        ?>
+        </section>
+        <div class="img-bg">
+            <img src="/Milana-tech-Circle-Solutions-Group-IT1B/root/images/Screenshot 2024-10-11 204828.png" alt="" class="img-bg">
+        </div>
+        <div class="about">
+            <div class="aboutheader">
+                On a mission to empower communicators worldwide
+            </div>
+            <div class="about-info">
+                <div class="OS-section">
+                    <div class="OS">
+                        OUR <p> STORY
+                    </div>
+                    <div class="OS-info1">
+                        Circle solution was founded in 2018 with the goal of equipping companies to reach every employee with inspiring communication — the key to impacting business success. More than 200 enterprises later, we are on a mission to help the world's best organizations inspire their people to reach their fullest potential, together. Our journey is about turning that inspired effort into extraordinary stories of success, change, and excellence.
+                    </div>
+                </div>
+          <div class="ctd-section">
+                <div class="ctd-info2">
+                    Our mission is to bridge communication and collaboration within organizations, ensuring that information flows seamlessly and effectively among all stakeholders.
+                </div>
+                <div class="ctd">
+                    CONNECTING THE DOTS
+                </div>
+            </div>
+            <div class="FR-section">
+                <div class="FR">
+                    FUTURE <p> READY
+                </div>
+                <div class="FR-info3">
+                    We stay updated with the latest technology trends, ensuring that your intranet remains relevant and capable of adapting to future demands.
+                </div>
+        </div>
+            <div class="cs-section">
+                <div class="cs-info4">
+                    Intranet designs that fit the specific needs of your organization, enhancing communication and collaboration.
+                </div>
+                <div class="cs">
+                    CUSTOM SOLUTIONS
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <div class="footer">
+            Footer section
+        </div>
+</body>
+</html>
---
+++ root/pages/product-workspace-large.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/global.css" type="text/css">
+</head>
+<body class="workspacebody">
+    <?php
+        include "../sections/header.html";
+    ?>
+    <div class="work">
+        <h1>Circle Workspace</h1>
+    </div>
+    <div class="container-workspace">
+        <div class="firstimage"><img src="../images/app-management.png" alt="app-management" class="workimage"></div>
+        <div class="secondimage"><img src="../images/communication-information.jpg" alt="communication-information" class="workimage"></div>
+        <div class="thirdimage"><img src="../images/knowledge-management.jpg" alt="knowledge-management" class="workimage"></div>
+        <div class="package"> 
+        <ul>
+            <li> Circle workspace centralizes documents from multiple sources, enabling seamless online and offline collaboration.</li>
+            <hr>
+            <li>Display information from applications directly in Circle Workspace. At a glance, see all your unread emails, <br>appointments, news, recently edited documents and more.</li>
+            <hr>
+            <li>Circle workspace simplifies workplace creation and administration, integrating current and future technologies in one platform.</li>
+        </ul>
+        </div>
+        <div class="plans">
+            <div class="gold"> <h1>Small Companies</h1>
+            <ul> <li> Document Management</li>
+                <li> Content Management</li>
+                <li> Analytics</li>
+                <li>Limited users (up to50)</li>
+            </ul> 
+            <input type="submit" value="Add to my cart" class="firstbutton">
+            <input type="submit" value="Buy now" class="secondbutton">
+        </div>
+            <div class="platinium"> <h1>Large Companies</h1>
+                <ul> <li> Document Management</li>
+                    <li> Content Management</li>
+                    <li> Analytics</li>
+                    <li>Limited users (up to500)</li>
+                </ul> 
+                <input type="submit" value="Add to my cart" class="firstbutton">
+                <input type="submit" value="Buy now" class="secondbutton">
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+    </div>
+
+</body>
+</html>
