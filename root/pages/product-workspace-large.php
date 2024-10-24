db6bfe86a40ffc1d8cbd01878b57a4e97c4ff23c
CosminBuzicUni <cosmin.buzic@student.nhlstenden.com>
2024-10-24 19:36:04 +0200

    fixed layout of workspace page(it affected the header and footer), fixed consistency of global.css; workspace page is not responsive, categories (small/large) have to be taken out of workspace and reviews have to be added

--- root/pages/product-workspace-large.php
+++ root/pages/product-workspace-large.php
@@ -1,59 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../css/global.css" type="text/css">
 </head>
-<body class="workspacebody">
-    <section class="navbar-workspace">
-        <?php
-            include "../sections/header.html";
-        ?>
-    </section>
-    <div class="work">
-        <h1>Circle Workspace</h1>
-    </div>
-    <div class="container-workspace">
-        <div class="firstimage"><img src="../images/app-management.png" alt="app-management" class="workimage"></div>
-        <div class="secondimage"><img src="../images/communication-information.jpg" alt="communication-information" class="workimage"></div>
-        <div class="thirdimage"><img src="../images/knowledge-management.jpg" alt="knowledge-management" class="workimage"></div>
-        <div class="package"> 
-        <ul>
-            <li> Circle workspace centralizes documents from multiple sources, enabling seamless online and offline collaboration.</li>
-            <hr>
-            <li>Display information from applications directly in Circle Workspace. At a glance, see all your unread emails, <br>appointments, news, recently edited documents and more.</li>
-            <hr>
-            <li>Circle workspace simplifies workplace creation and administration, integrating current and future technologies in one platform.</li>
-        </ul>
+
+<body>
+    <?php
+    include "../sections/header.html";
+    ?>
+    <div class="workspacebody">
+        <div class="work">
+            <h1>Circle Workspace</h1>
         </div>
-        <div class="plans">
-            <div class="gold"> <h1>Small Companies</h1>
-            <ul> <li> Document Management</li>
-                <li> Content Management</li>
-                <li> Analytics</li>
-                <li>Limited users (up to50)</li>
-            </ul> 
-            
-            <input type="submit" value="Buy now" class="secondbutton">
-        </div>
-            <div class="platinium"> <h1>Large Companies</h1>
-                <ul> <li> Document Management</li>
-                    <li> Content Management</li>
-                    <li> Analytics</li>
-                    <li>Limited users (up to500)</li>
-                </ul> 
-                
-                <input type="submit" value="Buy now" class="secondbutton">
+        <div class="container-workspace">
+            <div class="firstimage"><img src="../images/app-management.png" alt="app-management" class="workimage"></div>
+            <div class="secondimage"><img src="../images/communication-information.jpg" alt="communication-information" class="workimage"></div>
+            <div class="thirdimage"><img src="../images/knowledge-management.jpg" alt="knowledge-management" class="workimage"></div>
+            <div class="package">
+                <ul>
+                    <li> Circle workspace centralizes documents from multiple sources, enabling seamless online and offline collaboration.</li>
+                    <hr>
+                    <li>Display information from applications directly in Circle Workspace. At a glance, see all your unread emails, <br>appointments, news, recently edited documents and more.</li>
+                    <hr>
+                    <li>Circle workspace simplifies workplace creation and administration, integrating current and future technologies in one platform.</li>
+                </ul>
+            </div>
+            <div class="plans">
+                <div class="gold">
+                    <h1>Small Companies</h1>
+                    <ul>
+                        <li> Document Management</li>
+                        <li> Content Management</li>
+                        <li> Analytics</li>
+                        <li>Limited users (up to50)</li>
+                    </ul>
+
+                    <input type="submit" value="Buy now" class="secondbutton">
+                </div>
+                <div class="platinium">
+                    <h1>Large Companies</h1>
+                    <ul>
+                        <li> Document Management</li>
+                        <li> Content Management</li>
+                        <li> Analytics</li>
+                        <li>Limited users (up to500)</li>
+                    </ul>
+
+                    <input type="submit" value="Buy now" class="secondbutton">
+                </div>
             </div>
         </div>
-        <div class="footer">
-        <?php
-            include "../sections/footer.html";
-        ?>
-        </div>
     </div>
-
+    <?php
+    include "../sections/footer.html";
+    ?>
 </body>
+
 </html>
