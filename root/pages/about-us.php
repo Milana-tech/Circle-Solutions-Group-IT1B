3ec3babc07bffd8418c840789515cc41374a2a43
CosminBuzicUni <cosmin.buzic@student.nhlstenden.com>
2024-10-24 23:15:03 +0200

    added scripts to about-us and renamed it along with education and healthcare

--- root/pages/about-us.php
+++ root/pages/about-us.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Circle Solutions</title>
+    <script src="/root/scripts/header.js"></script>
     <link rel="stylesheet" href="../css/global.css" type="text/css">
 </head>
 <body class="aboutbody">
