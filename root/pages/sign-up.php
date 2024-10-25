1388c89816f0323289955075428933ab3ade44b7
Rodrigo <rodricardoso7@outlook.com>
2024-10-25 14:28:44 +0200

    Fixed my sign up to index php linking

--- root/pages/sign-up.php
+++ root/pages/sign-up.php
@@ -55,7 +55,7 @@ ob_start();
 
           if (!empty($name) && $email !== FALSE && !empty($password) && !empty($cPassword)) {
             if ($password === $cPassword) {
-              header("Location: http://localhost/products_rodrigo/Milana-tech-Circle-Solutions-Group-IT1B/root/index.php");
+              header("Location: ../index.php");
               exit();
             } elseif ($password != $cPassword) {
               echo "<p class = 'errors errorMsg'>Passwords do not match</p>";
