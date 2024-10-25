fbdc41df77a1b14adec1317d66ebef4020909de2
CosminBuzicUni <cosmin.buzic@student.nhlstenden.com>
2024-10-25 15:00:22 +0200

    moved contact us css in main

--- root/pages/checkout.php
+++ root/pages/checkout.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Check Out Page</title>
+    <script src="http://localhost/Milana-tech-Circle-Solutions-Group-IT1B/root/scripts/header.js"></script>
     <link rel="stylesheet" href="../css/global.css" type="text/css">
 </head>
 
