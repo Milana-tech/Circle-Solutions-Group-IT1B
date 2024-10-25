27668596841c0b64178fd3ca3fb818d0551337d8
CosminBuzicUni <cosmin.buzic@student.nhlstenden.com>
2024-10-25 03:40:11 +0200

    added title Circle Solutions, header.js and global.css to all of the current pages

--- root/pages/products-healthcare.php
+++ root/pages/products-healthcare.php
@@ -4,9 +4,9 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <script src="/root/scripts/header.js"></script>
-  <link rel="stylesheet" href="../css/global.css" type="text/css">
-  <title>Circle Solutions</title>
+    <title>Circle Solutions</title>
+    <script src="http://localhost/Milana-tech-Circle-Solutions-Group-IT1B/root/scripts/header.js"></script>
+    <link rel="stylesheet" href="../css/global.css" type="text/css">
 </head>
 
 <body class="healthcareEducationBody">
